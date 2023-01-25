437153981ef5b3e62265748b0bc2227227d085f2
hankh <hankh0412@naver.com>
2023-01-25 20:33:48 +0900

    test: promise

--- ch2/2.1.7-2.js
+++ ch2/2.1.7-2.js
@@ -1,5 +1,15 @@
+const condition = true; // true면 resolve, false면 reject
+const promise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('성공');
+  } else {
+    reject('실패');
+  }
+});
+
 promise
   .then((message) => {
+    console.log(message);
     return new Promise((resolve, reject) => {
       resolve(message);
     });
